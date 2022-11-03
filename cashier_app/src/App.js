49f1bc491dfaec64e13f4389ac56768eedd66b41
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-11-03 13:18:17 +0700

    install component dan membuat cashierq

---
+++ cashier_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavbarComponent from './component/NavbarComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent/>
+      <h2>Aloha</h2>
+    </div>
+  );
+}
+
+export default App;
